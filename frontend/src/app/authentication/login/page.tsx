3d9b94d54480fe292b198f77f351b0d744afb9a2
Aytsuu <paoloaraneta008@gmail.com>
2026-03-26 14:42:22 +0800

    Update redirect from auth for production

--- frontend/src/app/authentication/login/page.tsx
+++ frontend/src/app/authentication/login/page.tsx
@@ -191,7 +191,7 @@ export default () => {
               </div>
 
               <Link
-                href={`${api.defaults.baseURL}/api/auth/login-google?returnUrl=${api.defaults.baseURL}`}
+                href={`${api.defaults.baseURL}/api/auth/login-google?returnUrl=${process.env.NODE_ENV == "development" ? "http://localhost:3000" : process.env.NEXT_PUBLIC_API_URL}`}
                 className="bg-primary text-primary-foreground font-medium flex justify-center items-center gap-2 border w-full h-10 rounded-lg"
               >
                 <FcGoogle size={22} />
