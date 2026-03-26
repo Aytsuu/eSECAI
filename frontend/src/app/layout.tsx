d06f342604ef91f17a1882735a45d32c5442aa97
Aytsuu <paoloaraneta008@gmail.com>
2026-03-26 12:47:27 +0800

    Update

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,6 +6,7 @@ import { AuthProvider } from "../components/context/AuthContext";
 import { ThemeProvider } from "@/components/wrapper/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics/>
         <ThemeProvider attribute={"class"} defaultTheme="dark">
           <TooltipProvider>
             <Provider>
